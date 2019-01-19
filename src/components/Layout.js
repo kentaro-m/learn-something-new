8dd3c334e965dbb4f8dfb32f6856ac1cde012779
Kentaro Matsushita <kentaro.m811@gmail.com>
2019-01-20 01:15:15 +0900

    Adjust index page layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,7 +13,8 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            ...scale(1.5),
+            fontFamily: `Montserrat, sans-serif`,
+            ...scale(1.0),
             marginBottom: rhythm(1.5),
             marginTop: 0,
           }}
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,24 +18,27 @@ class BlogIndex extends React.Component {
           title="All posts"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          )
-        })}
+        <div style={{
+          marginBottom: rhythm(1.5),
+        }}>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <div key={node.fields.slug}>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+              </div>
+            )
+          })}
+        </div>
       </Layout>
     )
   }
