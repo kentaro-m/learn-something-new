4bfe03b7505890560d05bf344ec991ce49a52df2
kentaro-m <kentaro.m811@gmail.com>
2021-03-21 18:55:28 +0900

    Fix broken image

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { Flex, Box, Text, Link, Image } from '@chakra-ui/react'
 
 function Bio() {
@@ -8,14 +9,16 @@ function Bio() {
       query={bioQuery}
       render={data => {
         const { author } = data.site.siteMetadata
+        const image = getImage(data.avatar)
         return (
           <Flex justifyContent='center'>
             <Box mr={[3, 6]}>
-              <Image
-                src={data.avatar.childImageSharp.gatsbyImageData.src}
-                srcSet={data.avatar.childImageSharp.gatsbyImageData.srcSet}
+              {image && <Image
+                as={GatsbyImage}
+                image={image}
                 alt={author}
-              />
+                borderRadius='50%'
+              />}
             </Box>
             <Box>
               <Link href='https://kentarom.com/'>
@@ -38,7 +41,13 @@ function Bio() {
 const bioQuery = graphql`query BioQuery {
   avatar: file(absolutePath: {regex: "/avatar.jpeg/"}) {
     childImageSharp {
-      gatsbyImageData(width: 100, height: 100, layout: FIXED)
+      gatsbyImageData(
+        width: 100
+        height: 100
+        layout: FIXED
+        placeholder: BLURRED
+        formats: [AUTO, WEBP, AVIF]
+      )
     }
   }
   site {
