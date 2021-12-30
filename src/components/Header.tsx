70e13077ed93d71ae22b2a1c52ae080d18e0359e
kentaro-m <kentaro.m811@gmail.com>
2021-12-31 04:29:27 +0900

    style: update icon for blog title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { Link as GatsbyLink } from 'gatsby'
 import { Box, Link, Icon } from '@chakra-ui/react'
-import { FaLightbulb } from 'react-icons/fa'
+import { FaGhost } from 'react-icons/fa'
 
 const Header: React.FC = () => (
   <Box mt={10} textAlign='center'>
     <Link
       as={GatsbyLink}
       to='/'
-      color='gray.200'
+      color='purple.200'
       _hover={{
         textDecoration: 'none',
-        color: 'yellow.400', 
+        opacity: 0.8,
       }}
       fontSize='5xl'
       aria-label='ホーム'
     >
-      <Icon as={FaLightbulb}/>
+      <Icon as={FaGhost} />
     </Link>
   </Box>
 )
