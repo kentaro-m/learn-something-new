a3bf0c8fd63bbe65e382ba4c76b0677cf123252f
kentaro-m <kentaro.m811@gmail.com>
2022-02-19 22:06:13 +0900

    style: update header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import { Link as GatsbyLink } from 'gatsby'
-import { Box, Link, Icon } from '@chakra-ui/react'
-import { FaGhost } from 'react-icons/fa'
+import { Box, Link, Wrap } from '@chakra-ui/react'
 
 const Header: React.FC = () => (
-  <Box mt={10} textAlign='center'>
-    <Link
-      as={GatsbyLink}
-      to='/'
-      color='purple.200'
-      _hover={{
-        textDecoration: 'none',
-        opacity: 0.8,
-      }}
-      fontSize='5xl'
-      aria-label='ホーム'
-    >
-      <Icon as={FaGhost} />
-    </Link>
-  </Box>
+  <Wrap
+    justify='center'
+    mt={10}
+  >
+    <Box>
+      <Link
+        as={GatsbyLink}
+        to='/'
+        color='purple.200'
+        _hover={{
+          textDecoration: 'none',
+          opacity: 0.8,
+        }}
+        fontSize='2xl'
+        fontWeight={700}
+        fontFamily='Source Code Pro'
+      >
+        KNOWLEDGE_IS_POWER
+      </Link>
+    </Box>
+  </Wrap>
 )
 
 export default Header
