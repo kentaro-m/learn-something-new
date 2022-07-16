b716043746465014268250920d21638ccc94f56b
kentaro-m <kentaro.m811@gmail.com>
2022-07-17 01:02:37 +0900

    fix: add slide componrnt to mdx provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,11 @@ import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 import { MDXProvider } from '@mdx-js/react'
 import { theme } from './src/theme/theme'
 import { Tweet } from 'mdx-embed'
+import Slide from './src/components/Slide'
 
 const components = {
   Tweet,
+  Slide,
 }
 
 export const wrapRootElement = ({ element }) => (
